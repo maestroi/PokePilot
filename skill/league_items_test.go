c871c4514f8624004b0f4c3575ab3ce2ddb8260d
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-06 12:54:53 +0200

    league: remove duplicate PP selector test

--- skill/league_items_test.go
+++ skill/league_items_test.go
@@ -1,10 +1,6 @@
 package skill
 
-import (
-	"testing"
-
-	"github.com/maestroi/pokepilot/red/state"
-)
+import "testing"
 
 func TestLeagueItemIDsMatchGen1Constants(t *testing.T) {
 	if itemRevive != 0x35 || itemMaxRevive != 0x36 {
@@ -12,16 +8,5 @@ func TestLeagueItemIDsMatchGen1Constants(t *testing.T) {
 	}
 	if itemEther != 0x50 || itemMaxEther != 0x51 || itemElixer != 0x52 || itemMaxElixer != 0x53 {
 		t.Fatalf("PP restore ids = %#02x/%#02x/%#02x/%#02x, want 0x50..0x53", itemEther, itemMaxEther, itemElixer, itemMaxElixer)
-	}
-}
-
-func TestPPRestoreMoveSlotPrefersExhaustedMove(t *testing.T) {
-	mon := state.Mon{
-		Moves: [4]uint8{1, 2, 3, 0},
-		PP:    [4]uint8{5, 0, 1, 0},
-	}
-	slot, ok := ppRestoreMoveSlot(mon)
-	if !ok || slot != 1 {
-		t.Fatalf("ppRestoreMoveSlot = (%d,%v), want exhausted move slot 1", slot, ok)
 	}
 }
