13ebc3421312283cd625e37527a96cc9d6cff446
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-10 14:48:15 +0200

    agent: gate Saffron Gym on Silph rescue

--- agent/progression_gate.go
+++ agent/progression_gate.go
@@ -5,9 +5,10 @@ import "github.com/maestroi/pokepilot/red/state"
 const (
 	saffronCityMap uint8 = 0x0a
 	route2Map      uint8 = 0x0d
-	route3Map      uint8 = 0x0e
 	viridianGymMap uint8 = 0x2d
+	route3Map      uint8 = 0x0e
 	cinnabarGymMap uint8 = 0xa6
+	saffronGymMap  uint8 = 0xb2
 )
 
 func journeyProgressionBlocked(obs Observation, destinationMap uint8) bool {
@@ -18,6 +19,8 @@ func journeyProgressionBlocked(obs Observation, destinationMap uint8) bool {
 		return !observedEvent(obs, state.EventGotPokedex.String())
 	case saffronCityMap:
 		return !obs.Story.Has(ProgressSaffronGateOpen)
+	case saffronGymMap:
+		return !obs.Story.Has(redProgressSilphRescueComplete)
 	case cinnabarGymMap:
 		return !obs.Story.Has(ProgressSecretKeyOwned)
 	case viridianGymMap:
