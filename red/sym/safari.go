118d0e8b944599e29d5c943bd7f103e1d2d7ee80
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-14 12:18:24 +0200

    red: expose Safari ball count

---
+++ red/sym/safari.go
@@ -0,0 +1,6 @@
+package sym
+
+// NumSafariBalls is wNumSafariBalls. The Safari gate initializes it to 30;
+// Safari battle BALL decrements it directly because Safari Balls are not bag
+// items.
+const NumSafariBalls uint16 = 0xDA47
