329439a3e185c438699dad88d43707f8e7cf07bf
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-14 14:54:01 +0200

    dex: include NPC trades in Red objective menu

--- agent/tmhm.go
+++ agent/tmhm.go
@@ -25,6 +25,7 @@ func offerWithTMHM(m *emu.Emu, romData []byte, obs Observation, known *Knowledge
 	out = appendKnownCatchObjectives(romData, obs, known, out)
 	out = appendDexCatchObjectives(obs, known, out)
 	out = appendDexGiftObjectives(obs, known, out)
+	out = appendDexTradeObjectives(romData, obs, known, out)
 	out = appendDexEvolutionObjectives(obs, known, out)
 	var mem state.Mem
 	state.Snapshot(m, &mem)
