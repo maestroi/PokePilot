b8b96ba717fc457a6e05e97c93e3a30a05562660
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-15 17:05:58 +0200

    refactor(agent): carry adapter objective catalog on observations

--- agent/observe.go
+++ agent/observe.go
@@ -47,7 +47,8 @@ type Observation struct {
 
 	PokedexOwned []SpeciesID
 	PokedexSeen  []SpeciesID
-	Dex          DexCatalog `json:"-"`
+	Dex          DexCatalog        `json:"-"`
+	Catalog      ObjectiveCatalog  `json:"-"`
 
 	WildGrass  []WildSpecies
 	HasGrass   bool
