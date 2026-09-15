ce7f241dfecdf7dd0e66535d72740b8799d49b22
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-15 17:45:35 +0200

    agent: keep dex catch routing Red-native

--- agent/dex_catch.go
+++ agent/dex_catch.go
@@ -49,7 +49,7 @@ func appendDexCatchObjectives(obs Observation, known *Knowledge, out []Objective
 	hops := map[uint8]int{}
 	var adjacency map[uint8][]uint8
 	if known != nil {
-		adjacency = known.Adjacency
+		adjacency = known.nativeAdjacency()
 		hops = mapHops(adjacency, obs.Map)
 	}
 
