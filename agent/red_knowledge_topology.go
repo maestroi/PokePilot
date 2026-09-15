eb5183cd7d9ba9face8361a08b4f54eaa85d4224
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-15 17:20:26 +0200

    refactor(red): map native graph ids to semantic locations

---
+++ agent/red_knowledge_topology.go
@@ -0,0 +1,40 @@
+package agent
+
+import (
+	"fmt"
+
+	redprofile "github.com/maestroi/pokepilot/red/profile"
+	"github.com/maestroi/pokepilot/red/state"
+)
+
+func init() {
+	registerKnowledgeTopologyProvider(redprofile.GameID, &redObjectiveAdapter{})
+}
+
+func (a *redObjectiveAdapter) KnowledgeTopology(native map[uint8][]uint8) KnowledgeTopology {
+	topology := KnowledgeTopology{
+		Adjacency:       map[LocationID][]LocationID{},
+		NativeLocations: map[uint8]LocationID{},
+	}
+	location := func(id uint8) LocationID {
+		if known := topology.NativeLocations[id]; known != "" {
+			return known
+		}
+		name := state.MapName(id)
+		var semantic LocationID
+		if name != "" {
+			semantic = LocationID(semanticPlace(name))
+		} else {
+			semantic = LocationID(fmt.Sprintf("pokemon-red/map/%02x", id))
+		}
+		topology.NativeLocations[id] = semantic
+		return semantic
+	}
+	for from, neighbors := range native {
+		fromID := location(from)
+		for _, to := range neighbors {
+			topology.Adjacency[fromID] = append(topology.Adjacency[fromID], location(to))
+		}
+	}
+	return normalizeKnowledgeTopology(topology)
+}
