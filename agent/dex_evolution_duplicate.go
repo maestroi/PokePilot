fdcb6d9e38a2a4892cbdf967e8c9113084da5bae
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-15 17:47:31 +0200

    agent: keep duplicate evolution routing Red-native

--- agent/dex_evolution_duplicate.go
+++ agent/dex_evolution_duplicate.go
@@ -32,7 +32,7 @@ func appendDexDuplicateEvolutionBaseObjectives(obs Observation, known *Knowledge
 	hops := map[uint8]int{}
 	var adjacency map[uint8][]uint8
 	if known != nil {
-		adjacency = known.Adjacency
+		adjacency = known.nativeAdjacency()
 		hops = mapHops(adjacency, obs.Map)
 	}
 
