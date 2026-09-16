6a93a589cc3ab5447b7d1768c7bd11cd5a43ff37
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-16 15:07:50 +0200

    test(wall): expose coverage through run inspection finish view

---
+++ cmd/pokewall/run_coverage_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/maestroi/pokepilot/farm"
+)
+
+func TestRunFinishViewPreservesCoverage(t *testing.T) {
+	report := farm.FinishReport{
+		RunID: "completionist-coverage",
+		ProgressFinal: &farm.Progress{
+			Round: 17,
+			Coverage: &farm.Coverage{
+				UniqueMapsVisited: 9,
+				TrainersDefeated:  12,
+				NPCInteractions:   15,
+				DexOwned:          22,
+				TMsHMsUsed:        3,
+			},
+		},
+	}
+	data, err := json.Marshal(report)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var decoded farm.FinishReport
+	if err := json.Unmarshal(data, &decoded); err != nil {
+		t.Fatal(err)
+	}
+	view := runFinishView{
+		ProgressEarly: decoded.ProgressEarly,
+		ProgressFinal: decoded.ProgressFinal,
+	}
+	if view.ProgressFinal == nil || view.ProgressFinal.Coverage == nil {
+		t.Fatal("run inspection lost coverage")
+	}
+	if view.ProgressFinal.Coverage.TrainersDefeated != 12 || view.ProgressFinal.Coverage.DexOwned != 22 {
+		t.Fatalf("inspection coverage = %+v", view.ProgressFinal.Coverage)
+	}
+}
