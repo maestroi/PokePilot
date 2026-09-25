46cda1a416b440cf77528b179e83e0cffdba047f
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-25 19:18:18 +0200

    #974: drop obsolete battle ROM import

--- skill/battle.go
+++ skill/battle.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/maestroi/pokepilot/emu"
 	"github.com/maestroi/pokepilot/game"
-	"github.com/maestroi/pokepilot/red/rom"
 	"github.com/maestroi/pokepilot/red/state"
 	"github.com/maestroi/pokepilot/red/sym"
 )
