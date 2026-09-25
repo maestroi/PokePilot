f5cab0dee4836b7147d231764fd33770c87c2964
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-25 20:13:11 +0200

    #974: keep Red-only live-party helper out of Battle

--- skill/switch_policy_gen1_compat.go
+++ skill/switch_policy_gen1_compat.go
@@ -20,6 +20,10 @@ func bestReplacementSlot(romData []byte, mem *state.Mem, b state.BattleState) (i
 	return bestReplacementSlotState(romData, gen1BattleResourcesFromMem(mem), b)
 }
 
+func firstLivePartySlot(mem *state.Mem) int {
+	return gen1BattleResourcesFromMem(mem).FirstLivePartySlot()
+}
+
 func gen1BattleResourcesFromMem(mem *state.Mem) game.BattleResourcesState {
 	if mem == nil {
 		return game.BattleResourcesState{ActiveSlot: -1}
