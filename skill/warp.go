8dcba2e860b03ce65f09e10b8e67f31b12d1d9ff
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-25 23:57:41 +0200

    #974: gofmt routing/ROM lane

--- skill/warp.go
+++ skill/warp.go
@@ -7,10 +7,10 @@ import (
 
 	"github.com/maestroi/pokepilot/emu"
 	"github.com/maestroi/pokepilot/game"
-	"github.com/maestroi/pokepilot/worldmodel"
 	"github.com/maestroi/pokepilot/red/state"
 	"github.com/maestroi/pokepilot/red/sym"
 	"github.com/maestroi/pokepilot/world"
+	"github.com/maestroi/pokepilot/worldmodel"
 )
 
 // Budgets for the two phases of an edge crossing:
