1492defb4eb8830a3a41be23ac71533fce388815
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-25 22:20:05 +0200

    #974: gofmt overworld item slice

--- skill/field_item_architecture_test.go
+++ skill/field_item_architecture_test.go
@@ -9,7 +9,9 @@ import (
 func TestOverworldItemDriversDoNotImportConcreteGames(t *testing.T) {
 	for _, path := range []string{"field_item.go", "repel.go", "field_item_runtime.go"} {
 		src, err := os.ReadFile(path)
-		if err != nil { t.Fatalf("read %s: %v", path, err) }
+		if err != nil {
+			t.Fatalf("read %s: %v", path, err)
+		}
 		for _, forbidden := range []string{
 			"github.com/maestroi/pokepilot/red/",
 			"github.com/maestroi/pokepilot/blue/",
