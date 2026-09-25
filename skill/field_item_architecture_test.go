772307c7f3a882c1c2e4929e7e4223c0e75615a6
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-25 22:15:44 +0200

    #974: fence overworld item drivers from concrete games

---
+++ skill/field_item_architecture_test.go
@@ -0,0 +1,23 @@
+package skill
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestOverworldItemDriversDoNotImportConcreteGames(t *testing.T) {
+	for _, path := range []string{"field_item.go", "repel.go", "field_item_runtime.go"} {
+		src, err := os.ReadFile(path)
+		if err != nil { t.Fatalf("read %s: %v", path, err) }
+		for _, forbidden := range []string{
+			"github.com/maestroi/pokepilot/red/",
+			"github.com/maestroi/pokepilot/blue/",
+			"github.com/maestroi/pokepilot/yellow/",
+		} {
+			if strings.Contains(string(src), forbidden) {
+				t.Fatalf("%s imports concrete game package %q", path, forbidden)
+			}
+		}
+	}
+}
