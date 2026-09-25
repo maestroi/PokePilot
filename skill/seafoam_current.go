3a6765a9655f9859b0ba186fb825669d13c7ab05
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-26 00:01:12 +0200

    #974: remove stale Red ROM import

--- skill/seafoam_current.go
+++ skill/seafoam_current.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/maestroi/pokepilot/emu"
-	"github.com/maestroi/pokepilot/red/rom"
 	"github.com/maestroi/pokepilot/red/state"
 	"github.com/maestroi/pokepilot/red/sym"
 	"github.com/maestroi/pokepilot/world"
