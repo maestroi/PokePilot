3bb1cdb6609d1d7bde787d78a72d7fe58849aaee
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-25 11:45:15 +0200

    test(skill): pin Route 20 Secret Key resume exits

--- skill/cinnabar_secret_key_route_test.go
+++ skill/cinnabar_secret_key_route_test.go
@@ -64,3 +64,56 @@ func TestSecretKeyPalletRouteUsesRoute21(t *testing.T) {
 		t.Fatalf("Pallet -> Cinnabar route did not use Route 21 Surf: %+v", route)
 	}
 }
+
+
+func TestSecretKeyRoute20ResumeComponentsExitAwayFromSeafoam(t *testing.T) {
+	romData := badgeFourROM(t)
+
+	mem := fieldTestMem(FieldSurf, true, true, true)
+	mem[sym.ObtainedBadges] = 1<<state.BadgeBoulder | 1<<state.BadgeCascade |
+		1<<state.BadgeThunder | 1<<state.BadgeRainbow | 1<<state.BadgeSoul |
+		1<<state.BadgeMarsh
+	g, err := world.BuildGraph(romData)
+	if err != nil {
+		t.Fatalf("BuildGraph: %v", err)
+	}
+	prereqs := redRoutePrerequisites(g, romData, mem)
+	if !prereqs.Capabilities.Has(capCanSurf) {
+		t.Fatalf("capabilities missing %q: %v", capCanSurf, prereqs.Capabilities)
+	}
+
+	cinnabar := Destination{Map: cinnabarIslandMap, X: 11, Y: 12}
+	west, err := world.FindRoutePlanAtDestinationWithCapabilities(
+		g, route20Map, cinnabar.Map, 0, 10, int(cinnabar.X), int(cinnabar.Y), nil, prereqs,
+	)
+	if err != nil {
+		t.Fatalf("west Route 20 -> Cinnabar: %v", err)
+	}
+	if len(west) == 0 || west[0].Edge.To != cinnabarIslandMap {
+		t.Fatalf("west Route 20 did not exit directly to Cinnabar: %+v", west)
+	}
+	for _, step := range west {
+		if step.Edge.To == seafoam1FMap {
+			t.Fatalf("west Route 20 resume entered Seafoam: %+v", west)
+		}
+	}
+
+	fuchsia, ok := Place("fuchsia city")
+	if !ok {
+		t.Fatal("fuchsia city place missing")
+	}
+	east, err := world.FindRoutePlanAtDestinationWithCapabilities(
+		g, route20Map, fuchsia.Map, 99, 10, int(fuchsia.X), int(fuchsia.Y), nil, prereqs,
+	)
+	if err != nil {
+		t.Fatalf("east Route 20 -> Fuchsia: %v", err)
+	}
+	if len(east) == 0 || east[0].Edge.To != route19Map {
+		t.Fatalf("east Route 20 did not exit through Route 19: %+v", east)
+	}
+	for _, step := range east {
+		if step.Edge.To == seafoam1FMap {
+			t.Fatalf("east Route 20 resume entered Seafoam: %+v", east)
+		}
+	}
+}
