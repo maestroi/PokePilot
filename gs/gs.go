a7b9d5909105bbfec77b9642c4a96d86e59228e0
Chuck | Blockdaemon <chuck.veenvliet@protonmail.com>
2026-09-26 07:39:18 +0200

    gen2: expose Gold/Silver profile constructors

---
+++ gs/gs.go
@@ -0,0 +1,16 @@
+// Package gs exposes constructors for the supported Gold/Silver profiles.
+package gs
+
+import (
+	"github.com/maestroi/pokepilot/game"
+	gsprofile "github.com/maestroi/pokepilot/gs/profile"
+)
+
+const (
+	GoldGameID = gsprofile.GoldGameID
+	SilverGameID = gsprofile.SilverGameID
+)
+
+func New(ids ...game.GameID) *gsprofile.Profile { return gsprofile.New(ids...) }
+func Gold() *gsprofile.Profile { return gsprofile.NewGold() }
+func Silver() *gsprofile.Profile { return gsprofile.NewSilver() }
